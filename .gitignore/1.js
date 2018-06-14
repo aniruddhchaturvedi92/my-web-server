b60fbc991144259f678077c3991f46fa8edf7c79
aniruddhchaturvedi92 <38478695+aniruddhchaturvedi92@users.noreply.github.com>
2018-06-15 02:32:47 +0530

    Add files via upload

---
+++ .gitignore/1.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+
+
+var gulp = require('gulp');
+
+gulp.task('default', defaultTask);
+
+function defaultTask(done) {
+    // place code for your default task here
+    done();
+}
+
+app.use('/ani/:id', function(req, res, next) {
+    if (req.params.id == "vivek") {
+        next();
+    } else {
+        res.send("ACCESS DENIED");
+    }
+});
+
+app.get('/', function(request, response) {
+    response.send("hello");
+});
+
+
+app.get('/ani/:id', function(request, response) {
+    response.send("Welcome Master");
+});
+
+app.get('/vivek', function(request, response) {
+    response.send("vivek");
+});
+
+app.get('/aani/vivek', function(request, response) {
+    response.send("AV");
+});
+
+app.listen(1111, '0.0.0.0', function() {
+    console.log("Server Started");
+});
